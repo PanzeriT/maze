e7bb27206ba4930875382b8b9e5b4e74a56c4a00
Thomas Panzeri <th_panzeri@gmx.ch>
2020-01-09 19:20:06 +0100

    Add generic search functions

---
+++ search.py
@@ -0,0 +1,33 @@
+from __future__ import annotations
+from typing import TypeVar, Generic, List, Optional
+
+T: TypeVar = TypeVar('T')
+
+
+class Stack(Generic[T]):
+    def __init__(self) -> None:
+        self._container: List[T] = []
+
+    @property
+    def is_empty(self) -> bool:
+        return not self._container
+
+    def push(self, item: T) -> None:
+        self._container.append(item)
+
+    def pop(self) -> T:
+        return self._container.pop()
+
+    def __repr__(self) -> str:
+        return repr(self._container)
+
+
+class Node(Generic[T]):
+    def __init__(self, state: T, parent: Optional[Node], cost: float = 0.0, heuristic: float = 0.0):
+        self.state: T = state
+        self.parent: Optional[Node] = parent
+        self.cost: float = cost
+        self.heuristic: float = heuristic
+
+    def __lt__(self, other: Node) -> bool:
+        return (self.cost + self.heuristic) < (other.cost + other.heuristic)
